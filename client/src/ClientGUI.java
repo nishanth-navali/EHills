770b12cf75f3d4b76d4517ade4346ddffdc9655d
Nishanth Navali <nishanth.navali@utexas.edu>
2020-11-16 21:22:50 -0500

    organizational directory shifting
    moved everything into two projects, server and client

---
+++ client/src/ClientGUI.java
@@ -0,0 +1,57 @@
+package client;/*
+ * Author: Vallath Nandakumar and EE 422C instructors
+ * Date: November 9, 2020
+ * This starter code is from the MultiThreadChat example from the lecture, and is on Canvas. 
+ * It doesn't compile.
+ */
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+public class ClientGUI extends Application {
+	// I/O streams 
+	ObjectOutputStream toServer = null;
+	ObjectInputStream fromServer = null;
+
+
+	@Override
+	public void start(Stage primaryStage) { 
+		BorderPane mainPane = new BorderPane();
+
+		// Create a scene and place it in the stage 
+		Scene scene = new Scene(mainPane, 450, 200);
+		primaryStage.setTitle("Client"); // Set the stage title
+		primaryStage.setScene(scene); // Place the scene in the stage 
+		primaryStage.show(); // Display the stage 
+
+//		XX.setOnAction(e -> {
+//		});  // etc.
+
+		try { 
+			// Create a socket to connect to the server 
+			@SuppressWarnings("resource")
+			int port = 5000;
+			Socket socket = new Socket("localhost", port);
+
+			// Create an input stream to receive data from the server 
+			fromServer = new ObjectInputStream(socket.getInputStream()); 
+
+			// Create an output stream to send data to the server 
+			toServer = new ObjectOutputStream(socket.getOutputStream()); 
+		} 
+		catch (IOException ex) {
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
